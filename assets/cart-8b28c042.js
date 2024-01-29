62d25446651681402054d1811b850ed9893842a1
saeed-karout <saed.karot1@gmail.com>
2024-01-29 15:32:24 +0300

    adding dist

---
+++ assets/cart-8b28c042.js
@@ -0,0 +1,1 @@
+import{aF as s}from"./index-d93c72e8.js";const r=s("cart",{state:()=>({products:[]}),getters:{},actions:{AddToCart(t){this.products.push(t)},deleteProduct(t){this.products.splice(t,1)}},persist:!0});export{r as u};
---
+++ assets/logo-19d47908.js
@@ -0,0 +1,1 @@
+const s="/assets/logo-22570dcf.png";export{s as _};
